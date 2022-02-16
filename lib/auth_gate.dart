5fee7f0ab7ea0dd202bc3163e49ba99dee02befe
Pavan Kumar Kandapagari <pavan.kandapagari@agile-robots.com>
2022-02-16 13:33:38 +0100

    added clientId to secrets

--- lib/auth_gate.dart
+++ lib/auth_gate.dart
@@ -2,20 +2,19 @@ import 'package:first_app/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutterfire_ui/auth.dart';
+import 'package:first_app/secrets.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (context, snapshot) {
           if (!snapshot.hasData) {
-            return const SignInScreen(providerConfigs: [
-              EmailProviderConfiguration(),
-              GoogleProviderConfiguration(
-                  clientId: 'AIzaSyCWbwrIXbw_Twt9a10UwJ6xWkb9CJuNbgk')
+            return SignInScreen(providerConfigs: [
+              const EmailProviderConfiguration(),
+              GoogleProviderConfiguration(clientId: clientId)
             ]);
           }
           return const HomeScreen();
